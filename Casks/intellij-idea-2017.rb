865f4a16e457066af70a6fb66f034d67ee79f5e5
alswl <alswlx@gmail.com>
2020-04-01 00:53:01 +0800

    feat: idea 2017 for poor man

---
+++ Casks/intellij-idea-2017.rb
@@ -0,0 +1,28 @@
+cask 'intellij-idea-2017' do
+  version '2017.3.7'
+  sha256 '7c69bac5da70b4a165f76513ad6a5aa54140d8a9e72ae42d60e3367961f5e387'
+
+  url "https://download.jetbrains.com/idea/ideaIU-#{version}.dmg"
+  appcast 'https://data.services.jetbrains.com/products/releases?code=IIU&latest=true&type=release',
+          checkpoint: '00d60821b174bcca205cfb9b197e1b88900a27df3b90d4e8d42569619f472b38'
+  name 'IntelliJ IDEA Ultimate'
+  homepage 'https://www.jetbrains.com/idea/'
+
+  auto_updates true
+
+  app 'IntelliJ IDEA.app'
+
+  uninstall_postflight do
+    ENV['PATH'].split(File::PATH_SEPARATOR).map { |path| File.join(path, 'idea') }.each { |path| File.delete(path) if File.exist?(path) && File.readlines(path).grep(%r{# see com.intellij.idea.SocketLock for the server side of this interface}).any? }
+  end
+
+  zap trash: [
+               '~/Library/Preferences/com.jetbrains.intellij.plist',
+               "~/Library/Caches/IntelliJIdea#{version.major_minor}",
+               "~/Library/Logs/IntelliJIdea#{version.major_minor}",
+               "~/Library/Application Support/IntelliJIdea#{version.major_minor}",
+               "~/Library/Preferences/IntelliJIdea#{version.major_minor}",
+               '~/Library/Saved Application State/com.jetbrains.intellij.savedState',
+             ]
+end
+
