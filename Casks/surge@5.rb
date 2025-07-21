991f3d47eb0f2144e5eeb4766d1cb72556dc0c23
Jingchao <alswlx@gmail.com>
2025-07-21 01:46:09 +0000

    feat: surge@5

---
+++ Casks/surge@5.rb
@@ -0,0 +1,42 @@
+cask "surge@5" do
+  version "5.10.3,3272,5cf851de0c9af2bf96ab410244010f9a"
+  sha256 "3d10b83ed65448797d57a238729063abc6b704c183cffd51408ce54e87066079"
+
+  url "https://dl.nssurge.com/mac/v#{version.major}/Surge-#{version.tr(",", "-")}.zip"
+  name "Surge"
+  desc "Network toolbox"
+  homepage "https://nssurge.com/"
+
+  livecheck do
+    url "https://www.nssurge.com/mac/v#{version.major}/appcast-signed.xml"
+    regex(/[._-](\d+(?:\.\d+)+)[._-](\d+)[._-](\h+)\.zip/i)
+    strategy :sparkle do |item, regex|
+      match = item.url.match(regex)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]},#{match[3]}"
+    end
+  end
+
+  auto_updates true
+  conflicts_with cask:    [
+                   "surge",
+                   "surge@4",
+                 ],
+  depends_on macos: ">= :monterey"
+
+  app "Surge.app"
+
+  uninstall launchctl: "com.nssurge.surge-mac.helper",
+            delete:    "/Library/PrivilegedHelperTools/com.nssurge.surge-mac.helper"
+
+  zap delete: [
+    "~/Library/Application Support/com.nssurge.surge-mac",
+    "~/Library/Application Support/Surge",
+    "~/Library/Caches/com.nssurge.surge-mac*",
+    "~/Library/HTTPStorages/com.nssurge.surge-mac",
+    "~/Library/Logs/Surge",
+    "~/Library/Preferences/com.nssurge.surge*",
+    "~/Library/Saved Application State/com.nssurge.surge*",
+  ]
+end
